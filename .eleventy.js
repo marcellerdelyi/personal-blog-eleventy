c74d7085c4a878fb8b944ed1e43c74c085c2664c
marcellerdelyi <erdelyimarcell@gmail.com>
2024-04-28 14:18:10 +0200

    added Luxon plugin for formating dates, fixed broken link to about page and 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,18 @@
+// imports
+
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   // Add Passthrough Copy for CSS files
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css");
+
+  // FILTERS - Modify data in template files at build time
+  // Converts dates from JSDate format (Fri Dec 02 18:00:00 GMT-0600) to a locale format. https://11ty.rocks/eleventyjs/dates/
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  
 
   // Return Eleventy configuration options
   return {
@@ -11,3 +22,4 @@ module.exports = function (eleventyConfig) {
       },
   };
 };
+
--- src/blog.md
+++
@@ -1,6 +0,0 @@
----
-title: Blog
-layout: base.njk
----
-
-# Hello I'm the Blog site
