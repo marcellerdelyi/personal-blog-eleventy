e3906bc5909557831ffbc461fe95ccca420ac1d9
marcellerdelyi <erdelyimarcell@gmail.com>
2024-05-23 21:59:39 +0200

    changed location and config for site build

--- .eleventy.js
+++ .eleventy.js
@@ -34,7 +34,7 @@ module.exports = function (eleventyConfig) {
   return {
       dir: {
           input: "src",
-          output: "public",
+          output: "_site",
           data: "_data",     
       },
   };
